4273927eb5dab806151f09c13dd11d24726484a2
Bogdan Tirzioru <bogdan_tirzioru@yahoo.com>
2019-07-13 16:46:19 +0300

    delete generated files

--- firmware/powersupply/Debug/Src/string_proc.d
+++
@@ -1,7 +0,0 @@
-Src/string_proc.o: ../Src/string_proc.c \
- D:/gitRepos/Powersupply/firmware/powersupply/Inc/string_proc.h \
- D:/gitRepos/Powersupply/firmware/powersupply/Inc/types.h
-
-D:/gitRepos/Powersupply/firmware/powersupply/Inc/string_proc.h:
-
-D:/gitRepos/Powersupply/firmware/powersupply/Inc/types.h:
